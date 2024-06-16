9ed654216cd8af244ba45cb87e967bdd378aab71
Lakhveer-singh <67415026+Lakhveer-singh@users.noreply.github.com>
2024-06-16 21:55:08 +0530

    Add files via upload

---
+++ src/main/java/admin_user/AdminUserApplicationn.java
@@ -0,0 +1,13 @@
+package admin_user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminUserApplicationn {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminUserApplicationn.class, args);
+	}
+
+}
